ae6bb5bb5f98765c07d8a7a7b791d2de6459882f
yeshantharaka <elyeshantharaka@gmail.com>
2016-07-18 23:36:08 +0530

    add lab_1

---
+++ lab_1/min.c
@@ -0,0 +1,23 @@
+/**
+* Author 	: Mohammed Fawzan
+* Problem 	: If Then Else
+*/
+
+/**
+*	Find the minimum vlue from a given integer array
+* 	@param int* A 	pointeer to an integer array
+*	@param int n 	number of elements in the array
+*/
+int getMin(int *Array, int n) {
+  
+  int currmin = MAX_INT;
+  int i;
+  for (i=0; i<n; i++)
+    if (Array[i] > currmin)
+      currmin = Array[i];
+  return currmin;
+
+}
+
+
+
